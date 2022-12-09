969ee8da78646c8f0c1b741557daa5fa88aed939
NatiaGotsiridze <101630226+NatiaGo@users.noreply.github.com>
2022-12-10 01:53:27 +0400

    css html

--- script.js
+++ script.js
@@ -1,115 +1,114 @@
-'strict use'
+"strict use";
 
 let data = [
-    {
-      id: 1,
-      imageUrl: "../images/Rectangle.png",
-      title: "''It's no secret that Bali is the perfect destination for water lovers - if you're one of them, then don't lose this ticket to get the best watersport package here in Bali''",
-    },
-    {
-      id: 2,
-      imageUrl: "../images/Rectangle.png",
-      title: "''Don't miss Balinese massage! It's a combination of gentle stretches, acupressure, reflexology, and aromatherapy to stimulate the flow of blood and bring a sense of wellbeing''",
-    },
-    {
-      id: 3,
-      imageUrl: "../images/Rectangle.png",
-      title: "''Traveling means something different for everyone, but one thing most people have in common is their love for trying new cuisine. Fortunately, if you’re planning on visiting Bali, there are plenty of eats and drinks that are worth the journey, wherever your tastes may lie.''",
-    },
-  ];
-  
-  const arrowLeft = document.getElementById("arrow-left");
-  const arrowRight = document.getElementById("arrow-right");
-  const sliderContent = document.getElementById("slider-content");
-  let sliderIndex = 0;
-  let dotItem = document.getElementsByClassName('dot');
-  
-  
-  //div
-  function createDivTag() {
-    const divTag = document.createElement("div");
-    divTag.classList.add("slide");
-  
-    return divTag;
-  }
+  {
+    id: 1,
+    imageUrl: "../images/Rectangle.png",
+    title:
+      "''It's no secret that Bali is the perfect destination for water lovers - if you're one of them, then don't lose this ticket to get the best watersport package here in Bali''",
+  },
+  {
+    id: 2,
+    imageUrl: "../images/Rectangle.png",
+    title:
+      "''Don't miss Balinese massage! It's a combination of gentle stretches, acupressure, reflexology, and aromatherapy to stimulate the flow of blood and bring a sense of wellbeing''",
+  },
+  {
+    id: 3,
+    imageUrl: "../images/Rectangle.png",
+    title:
+      "''Traveling means something different for everyone, but one thing most people have in common is their love for trying new cuisine. Fortunately, if you’re planning on visiting Bali, there are plenty of eats and drinks that are worth the journey, wherever your tastes may lie.''",
+  },
+];
 
-  function createImgTag(item) {
-    const imgTag = document.createElement("div");
-    imgTag.style.background = `url(${item.imageUrl})`;
-    imgTag.classList.add('bg-imagee');
-  
-    return imgTag;
-  }
-  // title
-  function createTitleTag(item) {
-    const titleTag = document.createElement("h3");
-    titleTag.innerText = item.title;
-    titleTag.classList.add('titleOnPic')
-    return titleTag;
-  }
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+const sliderContent = document.getElementById("slider-content");
+let sliderIndex = 0;
+let dotItem = document.getElementsByClassName("dot");
 
-  // dots
-  function createDots() {
-    const dotsParent = document.createElement("div");
-    dotsParent.classList.add("dotParent");
-  
-  
-    data.forEach((Element) => {
-      const dot = document.createElement("div");
-      dot.classList.add("dot");
-      dot.setAttribute('data-id', Element.id - 1)
-      dotsParent.appendChild(dot);
-      dot.addEventListener('click', function(event){
-        let id = event.target.getAttribute('data-id');
-        sliderIndex = id;
-        slide();
-      })
+//div
+function createDivTag() {
+  const divTag = document.createElement("div");
+  divTag.classList.add("slide");
+
+  return divTag;
+}
+
+function createImgTag(item) {
+  const imgTag = document.createElement("div");
+  imgTag.style.background = `url(${item.imageUrl})`;
+  imgTag.classList.add("bg-imagee");
+
+  return imgTag;
+}
+// title
+function createTitleTag(item) {
+  const titleTag = document.createElement("h3");
+  titleTag.innerText = item.title;
+  titleTag.classList.add("titleOnPic");
+  return titleTag;
+}
+
+// dots
+function createDots() {
+  const dotsParent = document.createElement("div");
+  dotsParent.classList.add("dotParent");
+
+  data.forEach((Element) => {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dot.setAttribute("data-id", Element.id - 1);
+    dotsParent.appendChild(dot);
+    dot.addEventListener("click", function (event) {
+      let id = event.target.getAttribute("data-id");
+      sliderIndex = id;
+      slide();
     });
-    
-    return dotsParent;
-  }
-  
-  
-  function slide() {
-    sliderContent.innerHTML = " ";
-    const slideItem = createDivTag(data[sliderIndex]);
-    const imgTag = createImgTag(data[sliderIndex]);
-    const titleTag = createTitleTag(data[sliderIndex]);
-    const dotsElement = createDots();
-  
-    slideItem.appendChild(imgTag);
-    slideItem.appendChild(titleTag);
-    sliderContent.appendChild(slideItem);
-    sliderContent.appendChild(dotsElement);
-    dotItem[sliderIndex].classList.add('activeDot');
-  
-  }
-  
-  function arrowLeftClick() {
-    if (sliderIndex == 0) {
-      sliderIndex = data.length - 1;
-      slide();
-      return;
-    }
-    sliderIndex--;
+  });
+
+  return dotsParent;
+}
+
+function slide() {
+  sliderContent.innerHTML = " ";
+  const slideItem = createDivTag(data[sliderIndex]);
+  const imgTag = createImgTag(data[sliderIndex]);
+  const titleTag = createTitleTag(data[sliderIndex]);
+  const dotsElement = createDots();
+
+  slideItem.appendChild(imgTag);
+  slideItem.appendChild(titleTag);
+  sliderContent.appendChild(slideItem);
+  sliderContent.appendChild(dotsElement);
+  dotItem[sliderIndex].classList.add("activeDot");
+}
+
+function arrowLeftClick() {
+  if (sliderIndex == 0) {
+    sliderIndex = data.length - 1;
     slide();
+    return;
   }
-  
-  function arrowRightClick() {
-    if (sliderIndex == data.length - 1) {
-      sliderIndex = 0;
-      slide();
-      return;
-    }
-    sliderIndex++;
-    slide();
-  }
-  
-  arrowLeft.addEventListener("click", arrowLeftClick);
-  arrowRight.addEventListener("click", arrowRightClick);
-  
-  setInterval ( ()=> {
-      arrowRightClick();
-  }, 5000 );
-  
+  sliderIndex--;
   slide();
+}
+
+function arrowRightClick() {
+  if (sliderIndex == data.length - 1) {
+    sliderIndex = 0;
+    slide();
+    return;
+  }
+  sliderIndex++;
+  slide();
+}
+
+arrowLeft.addEventListener("click", arrowLeftClick);
+arrowRight.addEventListener("click", arrowRightClick);
+
+setInterval(() => {
+  arrowRightClick();
+}, 5000);
+
+slide();
